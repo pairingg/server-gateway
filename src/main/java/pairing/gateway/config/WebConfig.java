5588c1455b54a67aa47cdacc6f325b6b319e1b07
bbungjin <leejintwo12@naver.com>
2025-02-28 02:58:28 +0900

    fix: 헤더 중복

--- src/main/java/pairing/gateway/config/WebConfig.java
+++ src/main/java/pairing/gateway/config/WebConfig.java
@@ -1,21 +1,22 @@
-package pairing.gateway.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:3000",
-                        "https://pairing-prod.vercel.app",
-                        "https://pairing-dev.vercel.app",
-                        "https://pairing-henna.vercel.app",
-                        "https://pairing.swygbro.com"
-                )
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-}
+//package pairing.gateway.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    // CORS 설정 제거
+//    // @Override
+//    // public void addCorsMappings(CorsRegistry registry) {
+//    //     registry.addMapping("/**")
+//    //             .allowedOrigins(
+//    //                     "http://localhost:3000",
+//    //                     "https://pairing-prod.vercel.app",
+//    //                     "https://pairing-dev.vercel.app",
+//    //                     "https://pairing-henna.vercel.app",
+//    //                     "https://pairing.swygbro.com"
+//    //             )
+//    //             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+//    //             .allowedHeaders("*")
+//    //             .allowCredentials(true);
+//    // }
+//}
