424b90f18723489381158534653ffde1f2ce1358
bbungjin <leejintwo12@naver.com>
2025-03-05 19:22:57 +0900

    fix: 도메인 적용

--- src/main/java/pairing/gateway/config/WebSocketConfig.java
+++ src/main/java/pairing/gateway/config/WebSocketConfig.java
@@ -1,16 +1,16 @@
-package pairing.gateway.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
-@Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new ChatWebSocketHandler(), "/chat")
-                .setAllowedOrigins("*");  // 실제 운영에서는 특정 도메인만 허용
-    }
-}
+//package pairing.gateway.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.socket.config.annotation.EnableWebSocket;
+//import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+//import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+//
+//@Configuration
+//@EnableWebSocket
+//public class WebSocketConfig implements WebSocketConfigurer {
+//    @Override
+//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+//        registry.addHandler(new ChatWebSocketHandler(), "/chat")
+//                .setAllowedOrigins("*");  // 실제 운영에서는 특정 도메인만 허용
+//    }
+//}
